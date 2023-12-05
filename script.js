0c9922a6e3e597497dc12081c0e2e6317915f9ea
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 09:55:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  // Remove the # (if present) and convert to lowercase
+  shortHex = shortHex.replace("#", "").toLowerCase();
+
+  // Check if the input is a valid short hex code
+  if (/^[0-9a-f]{3}$/.test(shortHex)) {
+    // Convert each short component to a full component
+    const fullHex = shortHex
+      .split("")
+      .map((char) => char.repeat(2))
+      .join("");
+
+    // Add the # prefix to the full hex code
+    return "#" + fullHex;
+  } else {
+    // Handle invalid input
+    alert("Invalid short hex code.");
+  }
 };
 
 // Do not change the code below.
